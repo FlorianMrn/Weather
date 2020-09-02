88e280c244f5a69c2964840d8272ba2299a5ab8f
Florian Merrien <fmerrien.dev@gmail.com>
2020-09-02 19:24:49 +0200

    WeatherMiddleware config done

---
+++ src/redux/middlewares/weather.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import { GET_JSON } from '../actionsTypes';
+
+const weatherMiddleware = store => next => action => {
+    console.log("Middleware ok");
+    switch (action.type) {
+        case GET_JSON:
+            axios.post('url')
+                .then((res) => {
+
+                })
+                .catch((err) => {
+                    console.error(err)
+                }).finally(() => {
+
+                });
+            break;
+        default :
+        next(action);
+    };
+};
+
+export default weatherMiddleware;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux';
+
+// Local Import
 import input from './reducers/inputReducer';
 import weather from './reducers/weatherReducer';
+import weatherMiddleware from './middlewares/weather';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const enhancers = composeEnhancers(applyMiddleware(weatherMiddleware));
 
 export default createStore(combineReducers({
-    input,
-    weather}) , 
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    input, weather}), 
+    enhancers);
