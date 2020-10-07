cb49a015dc0b141af06237e8eb2c863eeca7a96c
Florian Merrien <fmerrien.dev@gmail.com>
2020-10-07 10:49:39 +0200

    All dependancies added and mongoDB connected

--- server/server.js
+++ server/server.js
@@ -3,13 +3,24 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello from express');
-});
+mongoose.connect(
+    `mongodb+srv://admin:${process.env.DB_PASS}@cluster0.o5plw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {   useNewUrlParser: true,
+        useUnifiedTopology: true 
+    }
+).then(() => console.log("MongoDB connected"))
+.catch((err) => console.log("MongoDB :", err));
 
 app.listen(PORT, () => {
     console.log(`Listenning on ${PORT}`);
