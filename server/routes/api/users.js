3145e47591449a5e12a8c0bed2288fd66c0a3bfb
Florian Merrien <fmerrien.dev@gmail.com>
2020-10-07 15:20:28 +0200

    Api routes Login and Register with passport and JWT added, verified with Postman

---
+++ server/routes/api/users.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../../models/User');
+
+router.post("/register", (req, res) => {
+    // Form Validation
+
+    User.findOne({username : req.body.username})
+    .then( user => {
+        if(user) {
+            res.status(400).json({username : "Ce nom d'utilisateur est déjà utilisé"})
+        } else {
+            const newUser = new User({
+                username: req.body.username,
+                password: req.body.password
+            });
+        
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+                if (err) throw err;
+                newUser.password = hash;
+                newUser
+                    .save()
+                    .then(user => res.json(user))
+                    .catch(err => console.log(err));
+            });
+        });
+        };
+    });
+});
+
+router.post("/login", (req, res) => {
+    
+    const {username, password} = req.body;
+
+    User.findOne({username})
+    .then(user => {
+        if(!user) {
+            return res.status(400).json({utilisateur : "utilisateur non trouvé"});
+        }
+
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                const payload = {
+                    id : user.id,
+                    username : user.username
+                }
+
+                jwt.sign(
+                    payload,
+                    process.env.SECRET,
+                    {
+                        expiresIn: 31556926 // One year in seconds
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json({password : "Mot de passe incorrect"});
+            }
+        });
+    });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const users = require('./routes/api/users');
 require('dotenv').config();
 
 app.use(
@@ -19,8 +21,14 @@ mongoose.connect(
     {   useNewUrlParser: true,
         useUnifiedTopology: true 
     }
-).then(() => console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB :", err));
+    ).then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log("MongoDB :", err));
+
+app.use(passport.initialize());
+
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
 
 app.listen(PORT, () => {
     console.log(`Listenning on ${PORT}`);
