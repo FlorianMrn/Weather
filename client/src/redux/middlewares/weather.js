1a2174d81196b5c054d8c0682803a1566d42c7a9
Florian Merrien <fmerrien.dev@gmail.com>
2020-10-12 17:00:27 +0200

    Create speciale instance for axios who delete authorization headers and works !

--- client/src/redux/middlewares/weather.js
+++ client/src/redux/middlewares/weather.js
@@ -11,12 +11,16 @@ const weatherMiddleware = store => next => action => {
     const state = store.getState();
     const input = state.input.value;
 
+    // S
+    const instance = axios.create();
+    delete instance.defaults.headers.common['Authorization'];
+
     switch (action.type) {
         case SEND_REQUEST_PIXA:
             
             const PIXA_KEY = process.env.REACT_APP_PIXA;
 
-            axios.get(`https://pixabay.com/api/?key=${PIXA_KEY}&q=${input} batiment&image_type=photo&lang=fr&per_page=3&SameSite=None`)
+            instance.get(`https://pixabay.com/api/?key=${PIXA_KEY}&q=${input} batiment&image_type=photo&lang=fr&per_page=3&SameSite=None`)
                 .then( (res) => {
                     store.dispatch(getPhoto(res.data.hits.length > 0 ? res.data.hits[0].largeImageURL : "https://hdwallpaperim.com/wp-content/uploads/2017/08/24/98616-minimalism-404_Not_Found-748x421.jpg"));
                 })
@@ -32,7 +36,7 @@ const weatherMiddleware = store => next => action => {
 
             const OPEN_KEY = process.env.REACT_APP_OPEN;
 
-            axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${OPEN_KEY}&units=metric&SameSite=None&lang=fr`)
+            instance.get(`https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${OPEN_KEY}&units=metric&SameSite=None&lang=fr`)
                 .then( (res) => {
                     store.dispatch(getJson(res.data));
                 })
