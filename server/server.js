115188a0bc8777d28dd6c4b8672f832ca4156b42
Florian Merrien <fmerrien.dev@gmail.com>
2020-10-15 11:50:39 +0200

    New test

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const path = require('path');
 require('dotenv').config();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(
     bodyParser.urlencoded({
