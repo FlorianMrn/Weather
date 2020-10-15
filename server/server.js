3c27b3f24d224394d5e6cb6e7c736235e02ab799
Florian Merrien <fmerrien.dev@gmail.com>
2020-10-15 14:48:35 +0200

    Test Heroku

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
   
     // index.html for all page routes
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve("https://serene-temple-02514.herokuapp.com/", "../client", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
     });
 };
   
